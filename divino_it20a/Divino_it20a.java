03ef628c1b02426e58fbd4ad8cb4d072c26a446a
meya <divinomeya@gmail.com>
2025-09-04 15:51:03 +0800

    Added a hello world class

---
+++ divino_it20a/Divino_it20a.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package divino_it20a;
+
+/**
+ *
+ * @author Admin
+ */
+public class Divino_it20a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
